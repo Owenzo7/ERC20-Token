2683722f14a20d8a1f3662ea596526cfcffd0c5b
Owenzo7 <owenlee545@gmail.com>
2023-03-15 15:48:45 +0300

    Enabled transfers between the sender and the recipient

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,6 +6,8 @@ contract ERC20 {
     uint256 public totalSupply;
     string public name;
     string public symbol;
+
+    mapping(address => uint256) public balanceOf;
     
 
 // To set the values of the 4 variables above.
@@ -18,4 +20,19 @@ constructor(string memory _name, string memory _symbol){
 function decimals() external pure returns (uint8) {
     return 18;
 }
+
+function transfer(address recipient, uint256 amount) external returns (bool) {
+
+require(recipient != address(0), "ERC20: transfer to the zero address");
+
+uint256 senderBalance = balanceOf[msg.sender];
+
+require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+
+balanceOf[msg.sender] = senderBalance - amount;
+
+balanceOf[recipient] += amount;
+
+}
+
 }
